6fa026c7d42e2b6324c3766e95fe25a6a1dfdc1c
antonvostrikov <42900827+antonvostrikov@users.noreply.github.com>
2024-11-03 01:32:36 +0300

    swiper-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css'
+import React from 'react'
+import  { Navigation, Thumbs, FreeMode, Autoplay } from 'swiper/modules';
+import SwiperCore from 'swiper'
+SwiperCore.use([Navigation,Thumbs]);
 export default function Home() {
+  const [thumbsSwiper, setThumbsSwiper] = React.useState<any>(null);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="slider">
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          thumbs={{ swiper: thumbsSwiper }}
+          navigation={true}
+          className="swiper-main"
+          autoplay={{delay: 3000}}
+          modules={[FreeMode, Navigation, Thumbs, Autoplay]}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <SwiperSlide className="swiper-main">
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="https://place-hold.it/500x500" alt="" width={500} height={500}/>
+          </SwiperSlide>
+        </Swiper>
+        <Swiper 
+          direction={'vertical'} 
+          className="swiper-thumbs" 
+          navigation={true} loop={true} 
+          onSwiper={setThumbsSwiper} 
+          slidesPerView={4} 
+          spaceBetween={34}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+        >    
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+          <SwiperSlide className="slide-thumb">
+            <Image src="https://place-hold.it/100x100" alt="" width={100} height={100}/>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
